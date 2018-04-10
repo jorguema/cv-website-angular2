75bcf41767a21b268e65e1f122d02e51fdaddd12
Jorge Guerola Martinez <jorguema@gmail.com>
2018-04-10 18:44:37 +0200

    sections & i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,62 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './structure/layout/layout.component';
+import { MainComponent } from './structure/main/main.component';
+import { HeaderComponent } from './structure/header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: AboutComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    MainComponent,
+    HeaderComponent,
+    AboutComponent,
+    ResumeComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
---
+++ src/app/resume/resume.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-resume',
+  templateUrl: './resume.component.html',
+  styleUrls: ['./resume.component.scss']
+})
+export class ResumeComponent {
+    
+}
