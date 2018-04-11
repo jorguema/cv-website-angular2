5c7929d0517b210d557d4e2a0d162be17a26af88
Jorge Guerola Martinez <jorguema@gmail.com>
2018-04-11 14:46:57 +0200

    images/scss/sidebar

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
-    
+  interests = [
+    'MUSIC',
+    'WEB_DEV',
+    'TRILOGIES',
+    'Exercise',
+    'STARTUPS',
+    'TEA',
+    'UX_UI',
+    'CATS',
+    'PRODUCTIVITY'
+  ];
 }
--- src/app/structure/header/header.component.ts
+++ src/app/structure/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'my-header',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  optionSelected = 'about';
+  router: Router;
+
+  constructor(private _router: Router) {
+    this.router = _router;
     
+    switch (this.router.url) {
+      case '/': { this.optionSelected = 'about'; break; }
+      case 'resume': { this.optionSelected = 'resume'; break; }
+      case 'experience': { this.optionSelected = 'experience'; break; }
+      case 'contact': { this.optionSelected = 'contact'; break; }
+      case 'skills': { this.optionSelected = 'skills'; break; }
+      default: break;
+    }
+  }
+
+  about() {
+    this.optionSelected = 'about';
+    this.router.navigate(['']);
+  };
+  resume() {
+    this.optionSelected = 'resume';
+    this.router.navigate(['resume']);
+  };
+  experience() {
+    this.optionSelected = 'experience';
+    this.router.navigate(['experience']);
+  };
+  contact() {
+    this.optionSelected = 'contact';
+    this.router.navigate(['contact']);
+  };
+  skills() {
+    this.optionSelected = 'skills';
+    this.router.navigate(['skills']);
+  };
 }
