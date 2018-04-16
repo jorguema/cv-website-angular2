c7aacc7bffc0215b8f005d538fa2a4d852acd200
Jorge Guerola Martinez <jorguema@gmail.com>
2018-04-16 17:57:08 +0200

    styles device

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+app.use(redirectUnmatched);
+
+function redirectUnmatched(req, res) {
+    res.redirect("http://jorgeguerola.com/");
+}
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
